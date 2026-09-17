e98207c0e8101451923efe2a8bc87cf2b197096f
nikol.nikolova <nikol.nikolova@egt-digital.com>
2026-09-17 16:29:18 +0300

    added missing pages in routes

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -4,13 +4,15 @@ import App from '../App';
 import { ResourceListPage } from '../admin/ResourceListPage';
 import { adminResources } from '../admin/resources';
 import { SidebarTitle } from '../components';
-import { AdminHomePage, UserHomePage, AboutPage } from '../pages';
+import { AdminHomePage, UserHomePage, AboutPage, GalleryPage, AccommondationPage } from '../pages';
 
 export const AppRoutes = () => (
   <Routes>
     <Route element={<App />}>
       <Route index element={<UserHomePage />} />
       <Route path="about" element={<AboutPage />} />
+      <Route path="gallery" element={<GalleryPage />} />
+      <Route path="accommondation" element={<AccommondationPage />} />
     </Route>
     <Route
       path="/admin"
